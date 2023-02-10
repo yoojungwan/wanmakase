404e2c4efe39c1aab73a3681659d06aac07a5084
yoojungwan <118862735+yoojungwan@users.noreply.github.com>
2023-02-10 17:24:21 +0900

    init commit

---
+++ src/main/java/org/wansun/wanmakase/WanmakaseApplication.java
@@ -0,0 +1,13 @@
+package org.wansun.wanmakase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanmakaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanmakaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wansun/wanmakase/WanmakaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wansun.wanmakase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanmakaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
